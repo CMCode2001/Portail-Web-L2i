c4f0afa1d2de52257c7162677eeaf33bdd990c30
bagherra99 <idydiagne1999@gmail.com>
2024-06-21 17:54:48 +0000

    mise en place des partnaires

---
+++ src/Components/partners.js
@@ -0,0 +1,35 @@
+import part1 from '../Assets/img/Partner1.jpg';
+import part2 from '../Assets/img/Partner2.jpg';
+import part4 from '../Assets/img/Partner4.jpg';
+import part5 from '../Assets/img/Partner5.jpg';
+import part6 from '../Assets/img/Partner6.png';
+import part7 from '../Assets/img/Partner7.jpg';
+
+const partners = [
+    {
+        name:'partner1',
+        path:part1
+    },
+    {
+        name:'partner2',
+        path:part2
+    },
+    {
+        name:'partner4',
+        path:part4
+    },
+    {
+        name:'partner5',
+        path:part5
+    },
+    {
+        name:'partner6',
+        path:part6
+    },
+    {
+        name:'partner7',
+        path:part7
+    }
+];
+
+export default partners;
