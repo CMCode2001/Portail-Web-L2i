c75217788eec77d63cd877dbd9a476f67dd701b7
eadarak <elabdoudrame2001@gmail.com>
2024-06-27 14:06:32 +0000

    a propos en phase

--- src/Components/_Components-Page-Apropos/components/Objectifs.jsx
+++ src/Components/_Components-Page-Apropos/components/Objectifs.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import icon from '../../../Assets/svg/valid-icon.svg';
 import "../../../Styles/Objectifs.css";
 
 function Objectifs() {
   return (
     <div class="objectifs container">
+      <div className="mission">
+        <div class="card" >
+        <img src={icon} alt="icon" />
+          <div class="card-body">
+            <h5 class="card-title">Former des professionnels qualifiés</h5>
+          </div>
+        </div>
+        <div class="card" >
+          <img src={icon} alt="icon" />
+          <div class="card-body">
+            <h5 class="card-title">Développer des compétences techniques</h5>
+          </div>
+        </div>
+        <div class="card" >
+        <img src={icon} alt="icon" />
+          <div class="card-body">
+            <h5 class="card-title">Favoriser l'innovation</h5>
+          </div>
+        </div>
+        <div class="card" >
+        <img src={icon} alt="icon" />
+          <div class="card-body">
+            <h5 class="card-title">Préparer a l’insertion professionnelle</h5>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
